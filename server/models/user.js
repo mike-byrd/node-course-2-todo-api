b4a1a0638ff020690e6b0b4900141909cdb477ef
Mike <michaelcbyrdjr@gmail.com>
2017-12-13 23:17:54 -0500

    Added /POST /users route and token method

--- server/models/user.js
+++ server/models/user.js
@@ -1,14 +1,61 @@
+const _ = require('lodash');
 const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
 
-//Defines a data model.
-var User = mongoose.model('User', {
+var UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
-    minLength: 1,
-    trim: true
+    minlength: 1,
+    trim: true,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: '{VALUE} is not a valid email'
+    }
   },
+  password: {
+    type: String,
+    require: true,
+    minlength: 6
+  },
+  tokens: [{
+    access: {
+      type: String,
+      required: true
+    },
+    token: {
+      type: String,
+      required: true
+    }
+  }]
+ }, {
+   usePushEach: true
+ });
+
+ UserSchema.methods.toJSON = function() {
+   var user = this;
+   var userObject = user.toObject();
+
+   return _.pick(userObject, ['_id','email']);
  }
-);
+
+ // Arrow functions do not bind a 'this' keyword. 'this' stores the indivirual document.
+ UserSchema.methods.generateAuthToken = function() {
+  var user = this;
+  var access = 'auth';
+  var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+
+  user.tokens = user.tokens.concat([{access, token}]);
+
+  // Returns the token value, as the success argument for the next 'then' call.
+  return user.save().then(() => {
+    return token;
+  });
+ }
+
+//Defines a data model.
+var User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
--- server/server.js
+++ server/server.js
@@ -96,7 +96,21 @@ app.patch('/todos/:id', (req, res) => {
     }).catch((e) => {
       res.status(400).send();
     }) 
-})
+});
+
+// POST /users
+app.post('/users', (req, res) => {
+  var user = new User(_.pick(req.body, ['email', 'password']));
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    // Prefixing a header with 'x-' means you're creating a custom header.
+    res.header('x-auth', token).send(user);
+  }).catch((err) => {
+    res.status(400).send(err);
+  });
+});
 
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
